769920fe83093d14299b2f82bce14b432e12e890
TaemHam <tk4003950@gmail.com>
2022-12-28 21:40:26 +0900

    #16 - 로그인 컨트롤러 테스트 + 시큐리티 설정 컨트롤러 테스트에 반영
    스프링 스큐리티가 자동 생성해준 `/login` 컨트롤러를 테스트
    모든 컨트롤러가 시큐리티 설정을 부를 수 있도록 `@Import` 추가

---
+++ src/main/java/com/practice/board/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.practice.board.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .formLogin().and()
+                .build();
+    }
+
+}
