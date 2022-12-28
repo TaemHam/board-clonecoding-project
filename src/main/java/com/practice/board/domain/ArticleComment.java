4262a774ef8f60d028e25d22d6f07f1d660abc95
TaemHam <tk4003950@gmail.com>
2022-12-28 12:15:21 +0900

    # 16 - 사용하지 않는 디펜던시 제거
    `Article`과 `ArticleComment`에 사용하지 않는 디펜던시 제거

--- src/main/java/com/practice/board/domain/ArticleComment.java
+++ src/main/java/com/practice/board/domain/ArticleComment.java
@@ -1,6 +1,5 @@
 package com.practice.board.domain;
 
-import java.time.LocalDateTime;
 import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -14,10 +13,6 @@ import javax.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
