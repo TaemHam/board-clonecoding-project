754666fae34157bdcd87a95bd0727acb4887cd01
TaemHam <tk4003950@gmail.com>
2022-12-28 21:38:58 +0900

    #16 - 시큐리티 설정을 모든 요청에 인증이 열리도록 수정
    아직 인증 기능 구현 단계에 이르지 않았으므로, 모든 인증 설정을 오픈한다.
    그리고 폼 로그인을 활성화해서 로그인 뷰가 그려지게끔 함
    
    `WebSecurityConfigurerAdapter`이 더 이상 사용하지 않게 되어, `SecurityFilterChain` 빈을 등록하는 방식으로 구현함
    
    * https://spring.io/blog/2022/02/21/spring-security-without-the-websecurityconfigureradapter

--- src/test/java/com/practice/board/controller/ArticleControllerTest.java
+++ src/test/java/com/practice/board/controller/ArticleControllerTest.java
@@ -6,15 +6,18 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import com.practice.board.config.SecurityConfig;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 @DisplayName("View 컨트롤러 - 게시글")
+@Import(SecurityConfig.class)
 @WebMvcTest(ArticleController.class)
 class ArticleControllerTest {
 
