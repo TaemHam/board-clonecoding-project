f90dd9f478e6ab96c9e4b1559d15e83bab646273
TaemHam <tk4003950@gmail.com>
2023-01-02 09:55:14 +0900

    #22 - 페이지네이션 기능을 컨트롤러(게시판 뷰)에 반영
    테스트는 `Page` 클래스를 이용하기 때문에
    하는 김에 페이징과 정렬을 함께 테스트
    나중에 정렬 구현에서 이어서 활용할 예정

--- src/main/java/com/practice/board/controller/ArticleController.java
+++ src/main/java/com/practice/board/controller/ArticleController.java
@@ -4,7 +4,10 @@ import com.practice.board.domain.type.SearchType;
 import com.practice.board.dto.response.ArticleResponse;
 import com.practice.board.dto.response.ArticleWithCommentsResponse;
 import com.practice.board.service.ArticleService;
+import com.practice.board.service.PaginationService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
@@ -21,6 +24,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class ArticleController {
 
     private final ArticleService articleService;
+    private final PaginationService paginationService;
 
     @GetMapping
     public String articles(
@@ -29,8 +33,11 @@ public class ArticleController {
             @PageableDefault(size = 10, sort = "createdAt", direction = Direction.DESC) Pageable pageable,
             ModelMap map
     ) {
-        map.addAttribute("articles", articleService.searchArticles(searchType, searchValue, pageable).map(
-                ArticleResponse::from));
+        Page<ArticleResponse> articles =  articleService.searchArticles(searchType, searchValue, pageable).map(
+                ArticleResponse::from);
+        List<Integer> barNumbers = paginationService.getPaginationBarNumbers(pageable.getPageNumber(), articles.getTotalPages());
+        map.addAttribute("articles", articles);
+        map.addAttribute("paginationBarNumbers", barNumbers);
 
         return "articles/index";
     }
