42e638ea31c8a695a7ed1d82fb78338726df808d
TaemHam <tk4003950@gmail.com>
2023-01-06 11:15:12 +0900

    #42 - 테스트 코드 오류 발견 및 수정
    전체 테스트를 다시 돌려보니 `TestSecurityConfig` 가 적용 안된 테스트가 있어 해결함

--- src/test/java/com/practice/board/controller/AuthControllerTest.java
+++ src/test/java/com/practice/board/controller/AuthControllerTest.java
@@ -1,25 +1,34 @@
 package com.practice.board.controller;
 
+import static org.mockito.BDDMockito.then;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.practice.board.config.SecurityConfig;
+import com.practice.board.config.TestSecurityConfig;
+import com.practice.board.service.ArticleService;
+import com.practice.board.service.PaginationService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 @DisplayName("View 컨트롤러 - 인증")
-@Import(SecurityConfig.class)
+@Import(TestSecurityConfig.class)
 @WebMvcTest(Void.class)
 public class AuthControllerTest {
 
     private final MockMvc mvc;
+
+    @MockBean
+    private ArticleService articleService;
+    @MockBean
+    private PaginationService paginationService;
 
     AuthControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
@@ -33,8 +42,9 @@ public class AuthControllerTest {
         // When & Then
         mvc.perform(get("/login"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
-                .andDo(MockMvcResultHandlers.print());
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+        then(articleService).shouldHaveNoInteractions();
+        then(paginationService).shouldHaveNoInteractions();
     }
 
 }
--- src/test/java/com/practice/board/controller/MainControllerTest.java
+++ src/test/java/com/practice/board/controller/MainControllerTest.java
@@ -1,38 +1,36 @@
 package com.practice.board.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-import com.practice.board.config.SecurityConfig;
+import com.practice.board.config.TestSecurityConfig;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
-@Import(SecurityConfig.class)
+@DisplayName("View 루트 컨트롤러")
+@Import(TestSecurityConfig.class)
 @WebMvcTest(MainController.class)
 class MainControllerTest {
 
     private final MockMvc mvc;
-
     public MainControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
     }
 
-
+    @DisplayName("[view][GET] 루트 페이지 -> 게시글 페이지")
     @Test
     void givenNothing_whenRequestingRootPage_thenRedirectsToArticlesPage() throws Exception {
         // Given
-
-        // When & Then
         mvc.perform(get("/"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("forward:/articles"))
-                .andExpect(forwardedUrl("/articles"))
-                .andDo(MockMvcResultHandlers.print());
+                .andExpect(forwardedUrl("/articles"));
     }
+
 }
